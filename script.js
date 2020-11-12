67826f0c40ffdc7629cb6270d4280bef27345bd9
Maciej <maciej.zelazo@protonmail.com>
2020-11-12 22:43:56 +0100

    nice animation with scale

---
+++ script.js
@@ -0,0 +1,43 @@
+let speed = 0;
+let position = 0;
+let rounded = 0;
+let block = document.getElementById('block');
+let elems = [...document.querySelectorAll('.n')];
+
+window.addEventListener('wheel', (e) => {
+    speed += e.deltaY * 0.0003;
+    //wheel works for the touch
+});
+
+let objs = Array(5).fill({
+    dist: 0
+});
+
+function raf() {
+    // console.log(speed);
+    position += speed;
+    speed *= 0.8; //ZWIEKSZENIE sprawi, że szybkość nie będzie spadać
+
+    objs.forEach((o, i) => {
+        o.dist = (position - i);
+        //position - i to jest wartość 0 i potem rośnie
+        o.dist = Math.abs(o.dist);
+        elems[i].style.transform = `scale(${o.dist})`;
+    });
+
+    rounded = Math.round(position);
+
+    let diff = (rounded - position);
+
+    // position += diff * 0.015;
+    position += Math.sign(diff) * Math.pow(Math.abs(diff), 0.7) * 0.015;
+    //druga propzoycja działa szybciej.
+
+
+
+    block.style.transform = `translate(0,${position*100}px`;
+    // raf - request animation frame
+    window.requestAnimationFrame(raf);
+}
+
+raf();
